38de02e4a0619386fd968388c62efb620e67ccb6
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-05-10 20:30:26 +0300

    add 3000

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mongoose
   .then(() => {
     console.log("Database connection successful");
     app.listen(PORT, () => {
-      console.log("Server is running. Use our API on port: 9000");
+      console.log("Server is running. Use our API on port: 3000");
     });
   })
   .catch((error) => {
