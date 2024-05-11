631f866e5362e0d4426b0a611af7739bd0125160
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-05-11 19:10:20 +0300

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -45,32 +45,3 @@ export const updateFavoriteSchema = Joi.object({
 });
 
 export const Contact = model("contact", contactSchema);
-
-// import { Schema, model } from "mongoose";
-// import handleMongooseError from "../helpers/handleMongooseError.js";
-
-// const contactSchema = new Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: [true, "Set name for contact"],
-//     },
-//     email: {
-//       type: String,
-//     },
-//     phone: {
-//       type: String,
-//     },
-//     favorite: {
-//       type: Boolean,
-//       default: false,
-//     },
-//   },
-//   { versionKey: false, timestamps: true }
-// );
-
-// contactSchema.post("save", handleMongooseError);
-
-// const Contact = model("contact", contactSchema);
-
-// export default Contact;
