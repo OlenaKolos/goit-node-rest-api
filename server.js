e0f43f23c3139c731eabc66fa383f8cebe4aca9c
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-05-12 20:30:07 +0300

    app.js, server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import app from "./app.js";
 
-const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST, PORT = 9000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
@@ -10,7 +10,7 @@ mongoose
   .then(() => {
     console.log("Database connection successful");
     app.listen(PORT, () => {
-      console.log("Server is running. Use our API on port: 3000");
+      console.log("Server is running. Use our API on port: 9000");
     });
   })
   .catch((error) => {
