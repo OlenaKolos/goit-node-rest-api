d546c547a06c0b53f8bdef6ae9ef5dbdad2965e4
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-05-16 17:27:34 +0300

    routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,35 +3,35 @@ import express from "express";
 import validateBody from "../helpers/validateBody.js";
 import {
   registerSchema,
-  //   loginSchema,
-  //   updateSubscriptionSchema,
+  loginSchema,
+  updateSubscriptionSchema,
 } from "../models/user.js";
 
 import {
   registerUser,
-  //   loginUser,
-  //   getCurrentUser,
-  //   logoutUser,
-  //   updateSubscription,
+  loginUser,
+  getCurrentUser,
+  logoutUser,
+  updateSubscription,
 } from "../controllers/auth.js";
 
-//import authenticate from "../middlewares/authenticate.js";
+import authenticate from "../middlewares/authenticate.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", validateBody(registerSchema), registerUser);
 
-// authRouter.post("/login", validateBody(loginSchema), loginUser);
+authRouter.post("/login", validateBody(loginSchema), loginUser);
 
-// authRouter.get("/current", authenticate, getCurrentUser);
+authRouter.get("/current", authenticate, getCurrentUser);
 
-// authRouter.post("/logout", authenticate, logoutUser);
+authRouter.post("/logout", authenticate, logoutUser);
 
-// authRouter.patch(
-//   "/subscription",
-//   authenticate,
-//   validateBody(updateSubscriptionSchema),
-//   updateSubscription
-// );
+authRouter.patch(
+  "/subscription",
+  authenticate,
+  validateBody(updateSubscriptionSchema),
+  updateSubscription
+);
 
 export default authRouter;
