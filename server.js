f9da2fbca92e4d7d64766af3aad310293a14552e
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-05-23 19:04:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,31 +17,3 @@ mongoose
     console.log(error.message);
     process.exit(1);
   });
-
-// import mongoose from "mongoose";
-// import app from "./app.js";
-// import findFreePort from "find-free-port";
-
-// const { DB_HOST, PORT = 9000 } = process.env;
-
-// mongoose.set("strictQuery", true);
-
-// findFreePort(PORT, (err, freePort) => {
-//   if (err) {
-//     console.error(err);
-//     process.exit(1);
-//   }
-
-//   mongoose
-//     .connect(DB_HOST)
-//     .then(() => {
-//       console.log("Database connection successful");
-//       app.listen(freePort, () => {
-//         console.log(`Server is running. Use our API on port: ${freePort}`);
-//       });
-//     })
-//     .catch((error) => {
-//       console.log(error.message);
-//       process.exit(1);
-//     });
-// });
